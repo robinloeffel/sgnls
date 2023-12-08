7948de8ae316d9c52a979db69ae617cffbb1703d
Robin Löffel <hi@robinloeffel.ch>
2023-12-08 09:57:34 +0100

    chore: remove dist files from repo

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: <T>(initialValue: T) => {
-    get: () => T;
-    set: (newValue: T) => void;
-    effect: (listener: (changedValue: T) => void) => void;
-    stop: () => void;
-};
-export default _default;
